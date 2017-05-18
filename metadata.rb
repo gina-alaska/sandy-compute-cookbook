db9dcbf916f10793a1d62a9da927e6c5f9bb3f03
Will Fisher <will@gina.alaska.edu>
2017-05-18 11:21:39 -0800

    bump cookbook version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'all_rights'
 description 'Installs/Configures sandy-compute'
 long_description 'Installs/Configures sandy-compute'
-version '0.3.0'
+version '0.4.0'
 
 depends 'lvm'
 depends 'xfs'
